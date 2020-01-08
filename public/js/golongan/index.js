f358a37fbe3fd2a889912c961d181c6830a0e953
Farhan <farhannfs5121@gmail.com>
2020-01-08 08:02:35 +0700

    getSessionError

--- public/js/golongan/index.js
+++ public/js/golongan/index.js
@@ -27,39 +27,7 @@ $(document).ready(function() {
 				}
 			}
 		}
-		$.ajax({
-			url: digitasLink + "/getSessionError",
-			type: "GET",
-			success: function(r){
-				if(r!=false){
-					var div = document.createElement("div");
-						$(div).attr("class","alert alert-danger alert-dismissible fade show");
-						$(div).attr("role","alert");
-						
-						var txt = document.createTextNode(r);
-						$(div).append(txt);
-						
-						var btn = document.createElement("button");
-							$(btn).attr("type","button");
-							$(btn).attr("class","close closeButtonPlease");
-							$(btn).attr("data-dismiss","alert");
-							$(btn).attr("aria-label","Close");
-							
-							var span = document.createElement("span");
-								$(span).attr("aria-hidden","true");
-								$(span).html("&times;");
-								$(btn).append(span);
-								
-						$(div).append(btn);
-						
-					$("div.container-category-alert").append(div);
-					var interval = setInterval(function(){
-						$("button.closeButtonPlease").click();
-						clearInterval(interval);
-					},10000);
-				}
-			}
-		});
+		getSessionError("div.container-category-alert");
 	} );
 	
 	function action(name, id){ // create form action
