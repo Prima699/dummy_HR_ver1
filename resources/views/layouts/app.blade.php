03df5794f4ab2f0a7bff177df39825f8c07e815c
Farhan Nur Fuadi <farhannfs5121@gmail.com>
2020-04-11 20:37:14 +0700

    meta

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,9 @@
     Digitas Information Sistem
   </title>
   <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+  <meta name="description" content="DIGITAS adalah sebuah sistem absensi karyawan dengan fitur deteksi wajah dan fitur Geo-location untuk melakukan absensi dari genggaman Anda.">
+  <meta name="keywords" content="DIGITAS,Ahura,attendance,digital,WFH,remote">
+  <meta name="author" content="Ahura">
   <!--     Fonts and icons     -->
   <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
